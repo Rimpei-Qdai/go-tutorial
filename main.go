f1cd7ca342c50141dfc8c0022ad092aba2e140a8
Rimpei-Qdai <hata.rimpei.319@s.kyushu-u.ac.jp>
2025-07-04 12:55:48 +0900

    ページを追加。getmethodから値を取得し計算

--- main.go
+++ main.go
@@ -9,8 +9,24 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "アクセスしたパス: %s", r.URL.Path)
 }
 
+func handler2(w http.ResponseWriter, r *http.Request) {
+	//getクエリパラメータから値を取得
+	name := r.URL.Query().Get("name")
+	stra := r.URL.Query().Get("a")
+	strb := r.URL.Query().Get("b")
+
+	var a, b int
+	fmt.Sscanf(stra, "%d", &a) // a に値を代入
+	fmt.Sscanf(strb, "%d", &b) // b に値を代入
+	c := a + b // a と b の合計を計算
+
+	fmt.Fprint(w, "name:", name)
+	fmt.Fprint(w, "a+b =", c)
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/add", handler2)
 	fmt.Println("http://localhost:8080 でサーバーが起動しました")
 	http.ListenAndServe(":8080", nil)
 }
